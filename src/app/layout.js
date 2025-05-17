53494836644f548186d6ae8aaf9a134b1b0ce951
Clenilson Brandão <77530995+klenilsonrox@users.noreply.github.com>
2025-05-17 14:06:35 -0300

    att

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,13 +5,14 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "KlenoDev desenvolvedor web",
-  description: "KlenoDev desenvolvedor fullstack",
+  title: "Klenilson Rox desenvolvedor web",
+  description: "KlenoDev desenvolvedor fullstack, klenilson rox clenilson brandao costa, klenilson brandão costa, clenilson brandão costa",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR" className="scroll-smooth">
+    <meta name="google-site-verification" content="w1_83JbvqVcyUOJ6m8ALp55C7VuhgMuDfH80DnHvy2s" />
         <link rel="icon" href="./images/favicon.png" />
       <body className={`${inter.className}`}>
       {children}
